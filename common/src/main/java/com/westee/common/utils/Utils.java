723449e7ca1bb5a7aa57827c1544b8c02ddc5492
dong <904885508@qq.com>
2024-01-25 18:26:49 +0800

    feat(common) 添加公用模块

---
+++ common/src/main/java/com/westee/common/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.westee.common.utils;
+
+import java.time.LocalDateTime;
+
+public class Utils {
+    public static LocalDateTime getNow() {
+        return LocalDateTime.now();
+    }
+}
