87855bba0b3d42bd31d6113f03b22160a6269824
dong <904885508@qq.com>
2024-02-26 22:11:42 +0800

    chores(openfeign) 删除多余代码。

--- openfeign/src/main/java/com/westee/openfeign/OpenfeignApplication.java
+++ openfeign/src/main/java/com/westee/openfeign/OpenfeignApplication.java
@@ -1,14 +1,11 @@
 package com.westee.openfeign;
 
-import com.westee.openfeign.client.RoleServiceProviderClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {RoleServiceProviderClient.class})
 public class OpenfeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpenfeignApplication.class, args);
--- openfeign/src/main/java/com/westee/openfeign/config/FeignConfig.java
+++ openfeign/src/main/java/com/westee/openfeign/config/FeignConfig.java
@@ -8,14 +8,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+/**
+ * FeignConfig实现了RequestInterceptor接口，用于在发起Feign请求时对请求进行拦截和处理。
+ * apply方法作用是：在调用openfeign定义的接口时，将token信息设置到header中，方便目标接口鉴权。
+ */
 @Configuration
 public class FeignConfig implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        System.out.println("yyyyyyyyyyyyyyyyyyyyyyy");
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxx");
         requestTemplate.header(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
     }
 }
