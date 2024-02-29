f55d6ab2e39f0b2e59b40704be9d07ff505b399c
dong <904885508@qq.com>
2024-02-29 20:47:13 +0800

    feat(openfeign) 在openfeign中添加获取用户id的接口。

--- openfeign/src/main/java/com/westee/openfeign/client/RoleServiceProviderClient.java
+++ openfeign/src/main/java/com/westee/openfeign/client/RoleServiceProviderClient.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
+/**
+ * 可以从eureka server中找到spring.application.name = auth-service的服务
+ * 以代替地址访问，如auth-service/api/v1/role，更加直观。
+ */
 @FeignClient(value = "auth-service", configuration = FeignConfig.class)
 public interface RoleServiceProviderClient {
 
@@ -18,5 +24,8 @@ public interface RoleServiceProviderClient {
 
     @RequestMapping(value = "/api/v1/user", method = RequestMethod.GET)
     Response<User> getUser(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String Token);
+
+    @RequestMapping(value = "/api/v1/users/ids", method = RequestMethod.GET)
+    Response<List<Long>> getUsersIds(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String Token);
 
 }
