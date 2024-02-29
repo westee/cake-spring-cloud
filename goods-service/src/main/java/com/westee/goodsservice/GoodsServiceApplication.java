87d52fc91f55cbb9640e3e271d183745834f131f
dong <904885508@qq.com>
2024-02-29 20:45:27 +0800

    feat(goods-service) 在使用openfeign的模块启动类上加@EnableFeignClients注解才能发现openfeign。

--- goods-service/src/main/java/com/westee/goodsservice/GoodsServiceApplication.java
+++ goods-service/src/main/java/com/westee/goodsservice/GoodsServiceApplication.java
@@ -2,8 +2,10 @@ package com.westee.goodsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.westee.openfeign.client")
 public class GoodsServiceApplication {
 
     public static void main(String[] args) {
