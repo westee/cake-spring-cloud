cda19a0a3baaf8ccd0081bf237979ce6993b3804
dong <904885508@qq.com>
2024-03-01 20:27:49 +0800

    feat(goods-service) 修改ShopController获取token，使用redis获取用户并鉴权。

--- goods-service/src/main/java/com/westee/goodsservice/controller/ShopController.java
+++ goods-service/src/main/java/com/westee/goodsservice/controller/ShopController.java
@@ -7,7 +7,6 @@ import com.westee.goodsservice.generate.Shop;
 import com.westee.goodsservice.service.ShopService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,30 +14,24 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ShopController {
     private final ShopService shopService;
-    private final RestTemplate restTemplate;
 
     @Autowired
-    public ShopController(ShopService shopService, RestTemplate restTemplate) {
+    public ShopController(ShopService shopService) {
         this.shopService = shopService;
-        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/shop")
     public PageResponse<Shop> getShop(@RequestParam(name = "pageNum", defaultValue = "1", required = false) Integer pageNum,
-                                      @RequestParam(name = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-                                      @RequestHeader("Token") String token) {
-        Long userId = getUserId(token);
-        return shopService.getShopsByUserId(pageNum, pageSize, userId);
+                                      @RequestParam(name = "pageSize", defaultValue = "10", required = false) Integer pageSize) {
+        return shopService.getShopsByUserId(pageNum, pageSize);
     }
 
     @GetMapping("/shop/{shopId}")
@@ -47,32 +40,22 @@ public class ShopController {
     }
 
     @PostMapping("/shop")
-    public Response<Shop> createShop(@RequestBody Shop shop, HttpServletResponse response,
-                                     @RequestHeader("Token") String token) {
-        Long userId = getUserId(token);
-        Response<Shop> ret = Response.of(ResponseMessage.OK.toString(), shopService.createShop(shop, userId));
+    public Response<Shop> createShop(@RequestBody Shop shop, HttpServletResponse response) {
+        Response<Shop> ret = Response.of(ResponseMessage.OK.toString(), shopService.createShop(shop));
         response.setStatus(HttpStatus.CREATED.value());
         return ret;
     }
 
     @PatchMapping("/shop/{id}")
     public Response<Shop> updateShop(@PathVariable("id") Long id,
-                                     @RequestBody Shop shop,
-                                     @RequestHeader("Token") String token) {
+                                     @RequestBody Shop shop) {
         shop.setId(id);
-        Long userId = getUserId(token);
-        return Response.of(ResponseMessage.OK.toString(),shopService.updateShop(shop, userId));
+        return Response.of(ResponseMessage.OK.toString(),shopService.updateShop(shop));
     }
 
     @DeleteMapping("/shop/{id}")
-    public Response<Shop> deleteShop(@PathVariable("id") Long shopId,
-                                     @RequestHeader("Token") String token) {
-        Long userId = getUserId(token);
-        return Response.of(ResponseMessage.OK.toString(), shopService.deleteShop(shopId, userId));
+    public Response<Shop> deleteShop(@PathVariable("id") Long shopId) {
+        return Response.of(ResponseMessage.OK.toString(), shopService.deleteShop(shopId));
     }
 
-    public Long getUserId(String token) {
-        restTemplate.exchange("1L", HttpMethod.GET, null, Long.class);
-        return null;
-    }
 }
--- goods-service/src/main/java/com/westee/goodsservice/service/ShopService.java
+++ goods-service/src/main/java/com/westee/goodsservice/service/ShopService.java
@@ -1,13 +1,6 @@
 package com.westee.goodsservice.service;
 
 import com.github.pagehelper.PageHelper;
-import com.westee.cake.entity.GoodsStatus;
-import com.westee.cake.entity.PageResponse;
-import com.westee.cake.exceptions.HttpException;
-import com.westee.cake.generate.Shop;
-import com.westee.cake.generate.ShopExample;
-import com.westee.cake.generate.ShopMapper;
-import com.westee.cake.util.Utils;
 import com.westee.common.entity.PageResponse;
 import com.westee.common.exception.HttpException;
 import com.westee.common.utils.Utils;
@@ -15,6 +8,7 @@ import com.westee.goodsservice.entity.ShopStatus;
 import com.westee.goodsservice.generate.Shop;
 import com.westee.goodsservice.generate.ShopExample;
 import com.westee.goodsservice.generate.ShopMapper;
+import com.westee.goodsservice.redis.UserRedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +17,19 @@ import java.util.Objects;
 
 @Service
 public class ShopService {
+
     private final ShopMapper shopMapper;
+    private final UserRedisService userRedisService;
 
     @Autowired
-    public ShopService(ShopMapper mapper) {
+    public ShopService(ShopMapper mapper, UserRedisService userRedisService) {
         this.shopMapper = mapper;
+        this.userRedisService = userRedisService;
     }
 
-    public PageResponse<Shop> getShopsByUserId(Integer pageNum, Integer pageSize, long userId) {
+    public PageResponse<Shop> getShopsByUserId(Integer pageNum, Integer pageSize) {
         ShopExample shopExample = new ShopExample();
-        shopExample.createCriteria().andOwnerUserIdEqualTo(userId).andStatusEqualTo(ShopStatus.OK.getName());
+        shopExample.createCriteria().andOwnerUserIdEqualTo(getUserId()).andStatusEqualTo(ShopStatus.OK.getName());
         long count = shopMapper.countByExample(shopExample);
         long totalPage = count % pageSize == 0 ? count / pageSize : count / pageSize + 1;
         PageHelper.startPage(pageNum, pageSize);
@@ -40,52 +37,57 @@ public class ShopService {
         return PageResponse.pageData(pageNum, pageSize, totalPage, shops);
     }
 
-    public Shop createShop(Shop shop, long userId) {
-        shop.setOwnerUserId(userId);
+    public Shop createShop(Shop shop) {
+        shop.setOwnerUserId(getUserId());
         shop.setId(null);
         shop.setStatus(ShopStatus.OK.getName());
         shop.setUpdatedAt(Utils.getNow());
         shop.setCreatedAt(Utils.getNow());
-        int shopId = shopMapper.insertSelective(shop);
-        shop.setId((long) shopId);
+        shopMapper.insertSelective(shop);
         return shop;
     }
 
-    public Shop updateShop(Shop shop, long userId) {
-        Shop shopResult = shopMapper.selectByPrimaryKey(shop.getId());
-        if (Objects.equals(shopResult, null)) {
-            throw HttpException.forbidden("参数不合法");
+    public Shop updateShop(Shop shop) {
+        if (Objects.isNull(shop.getId())) {
+            throw HttpException.forbidden("店铺参数不合法");
         }
-        Long ownerUserId = shopResult.getOwnerUserId();
-        if (Objects.equals(ownerUserId, userId)) {
-            shop.setUpdatedAt(Utils.getNow());
-            shop.setCreatedAt(Utils.getNow());
-            shopMapper.updateByPrimaryKeySelective(shop);
-            return shop;
-        } else {
-            throw HttpException.forbidden("拒绝访问");
-        }
+        checkAuth(shop.getId());
+
+        shop.setUpdatedAt(Utils.getNow());
+        shop.setCreatedAt(Utils.getNow());
+        shopMapper.updateByPrimaryKeySelective(shop);
+        return shop;
     }
 
-    public Shop deleteShop(Long shopId, long userId) {
-        Shop shop = shopMapper.selectByPrimaryKey(shopId);
-        if (Objects.equals(shop, null)) {
-            throw HttpException.forbidden("参数不合法");
-        }
-        Long ownerUserId = shop.getOwnerUserId();
-        if (Objects.equals(ownerUserId, userId)) {
-            shop.setStatus(ShopStatus.DELETED.getName());
-            shop.setUpdatedAt(Utils.getNow());
-            shop.setCreatedAt(Utils.getNow());
-            shopMapper.updateByPrimaryKeySelective(shop);
-            return shop;
-        } else {
-            throw HttpException.forbidden("拒绝访问");
-        }
+    public Shop deleteShop(Long shopId) {
+        checkAuth(shopId);
+        Shop shop = new Shop();
+        shop.setId(shopId);
+        shop.setStatus(ShopStatus.DELETED.getName());
+        shop.setUpdatedAt(Utils.getNow());
+        shop.setCreatedAt(Utils.getNow());
+        shopMapper.updateByPrimaryKeySelective(shop);
+        return shop;
     }
 
     public Shop getShopByShopId(long shopId) {
         return shopMapper.selectByPrimaryKey(shopId);
+    }
+
+    public Long getUserId() {
+        return userRedisService.getUser().getId();
+    }
+
+    private void checkAuth(Long shopId) {
+        Shop shopResult = shopMapper.selectByPrimaryKey(shopId);
+        if (Objects.equals(shopResult, null)) {
+            throw HttpException.forbidden("参数不合法");
+        }
+
+        Long ownerUserId = shopResult.getOwnerUserId();
+        if (!Objects.equals(ownerUserId, getUserId())) {
+            throw HttpException.forbidden("拒绝访问");
+        }
     }
 
 }
